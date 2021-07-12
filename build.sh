8715ab5f12161ecd84d97eaa8c63ec82850940f1
hans <hans@dkmt.io>
2021-07-12 12:32:32 +0800

    support both configure.py and off source build
    Signed-off-by: hans <hans@dkmt.io>

--- build.sh
+++ build.sh
@@ -1,5 +1,19 @@
 #!/usr/bin/env sh
 
+if [ -f "./configure.py" ] ; then
+
+./configure.py \
+--compiler="gcc-10" \
+--cflags="-fcoroutines" \
+--ldflags="-lstdc++ -lm"
+
+cmake --build build/dev
+cmake --build build/debug
+cmake --build build/release
+cmake --build build/sanitize
+
+else
+
 cmake \
 -DCMAKE_CXX_COMPILER="gcc-10" \
 -DSeastar_CXX_FLAGS="-Wno-stringop-overflow;-fcoroutines" \
@@ -7,3 +21,5 @@ cmake \
 -B build seastar
 
 cmake --build build
+
+fi
